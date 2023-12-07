d5b7cc992093d0bd839bf6d700f9448bea7b6261
Joe Masri <zelma760@mtroyal.ca>
2023-12-07 15:02:24 -0700

    More progress towards displaying clicked song

--- js-files/SearchBrowse.js
+++ js-files/SearchBrowse.js
@@ -115,6 +115,13 @@ function displaySongs(songsToDisplay) {
             	// get the song object from the id
                 const selectedSong = songsToDisplay.find( s=> s.song_id == id)
                 console.log(selectedSong);
+
+                // display the song details
+                document.querySelector("#title").textContent = selectedSong.title;
+                document.querySelector("#energy").textContent = selectedSong.analytics.energy;
+                document.querySelector("#danceablitiy").textContent = selectedSong.analytics.danceability;
+                document.querySelector("#liveness").textContent = selectedSong.analytics.liveness;
+    
         })        
     });
 }
