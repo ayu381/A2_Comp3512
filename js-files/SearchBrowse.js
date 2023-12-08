e6a90d8c3c14f815bfaba8d4c18db91fce3d16ab
Joe Masri <zelma760@mtroyal.ca>
2023-12-08 14:22:25 -0700

    moved functionality of row clicked to other js file

--- js-files/SearchBrowse.js
+++ js-files/SearchBrowse.js
@@ -111,22 +111,6 @@ function displaySongs(songsToDisplay) {
     });
 }
 
-function rowClicked(song, songsToDisplay) {
-        //extract the song_id
-        const id = song.song_id;
-        console.dir("song id=" + id);
-
-            // get the song object from the id
-            const selectedSong = songsToDisplay.find(s => s.song_id == id);
-            console.log(selectedSong);
-
-            // display the song details
-            document.querySelector("#title").textContent = selectedSong.title;
-            document.querySelector("#energy").textContent = selectedSong.analytics.energy;
-            document.querySelector("#danceablitiy").textContent = selectedSong.analytics.danceability;
-            document.querySelector("#liveness").textContent = selectedSong.analytics.liveness;
-}
-
 function addToPlaylist(song) {
     // Implement the logic to add the song to the playlist
     // For example, you can store the selected songs in an array or perform any other desired action
